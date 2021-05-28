0169bad06fa42ef43c62e3c4419fc57b10a93beb
ErickFelipe12 <erick.felipep12@gmail.com>
2021-05-28 17:23:34 -0500

    Consultando datos de la tabla usuario

---
+++ Repositorio1/Controllers/UsuarioController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+namespace Repositorio1.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db= new inventario2021Entities1())
+            {
+                return View(db.usuario.ToList());
+            }
+                
+        }
+    }
+}
