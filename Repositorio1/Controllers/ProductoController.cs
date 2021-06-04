b859f3dda27c9db2089d24042ffe34f06606792d
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-04 15:39:59 -0500

    Controlador producto

---
+++ Repositorio1/Controllers/ProductoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+
+namespace Repositorio1.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return View();
+            }
+        }
+
+        public static string Nombreproveedor(int idProveedor)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return db.producto.Find(idProveedor).nombre;
+            }
+        }
+    }
+}
