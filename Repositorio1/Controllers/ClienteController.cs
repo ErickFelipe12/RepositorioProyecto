6edce7624141cddb7fda7ab83d45d0ef7535bb9b
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-05 16:57:01 -0500

    CRUD cliente Completo

---
+++ Repositorio1/Controllers/ClienteController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+
+namespace Repositorio1.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    cliente findUser = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente clienteEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    usuario user = db.usuario.Find(clienteEdit.id);
+
+                    user.nombre = clienteEdit.nombre;
+                    user.apellido = clienteEdit.documento;
+                    user.email = clienteEdit.email;
+                    
+                   
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+        public ActionResult Details(int id)
+        {//abriendo la conexion a la BD
+            using (var db = new inventario2021Entities1())
+            {
+                //buscar usuario por id
+                cliente user = db.cliente.Find(id);
+                return View(user);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                var cliente = db.cliente.Find(id);
+                db.cliente.Remove(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
