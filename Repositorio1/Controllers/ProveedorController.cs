3e59a9d36f8b5b35396f76572f867b8778caa3d1
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-05 17:59:49 -0500

    CRUD proveedor completo

---
+++ Repositorio1/Controllers/ProveedorController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+
+namespace Repositorio1.Controllers
+{
+    public class ProveedorController : Controller
+    {
+        // GET: Proveedor
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return View(db.proveedor.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(proveedor proveedor)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    db.proveedor.Add(proveedor);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    proveedor findUser = db.proveedor.Where(a => a.id == id).FirstOrDefault();
+                    return View(findUser);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(proveedor proveedorEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    proveedor user = db.proveedor.Find(proveedorEdit.id);
+
+                    user.nombre = proveedorEdit.nombre;
+                    user.direccion = proveedorEdit.direccion;
+                    user.telefono = proveedorEdit.telefono;
+                    user.nombre_contacto= proveedorEdit.nombre_contacto;
+
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+
+        }
+        public ActionResult Details(int id)
+        {//abriendo la conexion a la BD
+            using (var db = new inventario2021Entities1())
+            {
+                //buscar usuario por id
+                proveedor user = db.proveedor.Find(id);
+                return View(user);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                var proveedor = db.proveedor.Find(id);
+                db.proveedor.Remove(proveedor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
