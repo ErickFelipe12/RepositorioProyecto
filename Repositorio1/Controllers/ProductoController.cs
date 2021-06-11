80a98b39535ccbc6dc0c06efafae98e7e4bc9fc8
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-11 01:43:39 -0500

    CRUD producto completo

--- Repositorio1/Controllers/ProductoController.cs
+++ Repositorio1/Controllers/ProductoController.cs
@@ -14,7 +14,7 @@ namespace Repositorio1.Controllers
         {
             using (var db = new inventario2021Entities1())
             {
-                return View();
+                return View(db.producto.ToList());
             }
         }
 
@@ -22,7 +22,78 @@ namespace Repositorio1.Controllers
         {
             using (var db = new inventario2021Entities1())
             {
-                return db.producto.Find(idProveedor).nombre;
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+        public ActionResult ListarProveedores()
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(producto newProducto)
+        {
+            if (ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    db.producto.Add(newProducto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                var producto = db.producto.Find(id);
+                db.producto.Remove(producto);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                producto productoDetalle = db.producto.Where(a => a.id == id).FirstOrDefault();
+                return View(productoDetalle);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db= new inventario2021Entities1())
+                {
+                    producto producto = db.producto.Where(a => a.id == id).FirstOrDefault();
+                    return View(producto);
+
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("", "ERROR" + ex);
+                    return View();
             }
         }
     }
