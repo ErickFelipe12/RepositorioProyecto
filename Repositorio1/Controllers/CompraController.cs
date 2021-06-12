8e6f6813732f2a82a12327c871f12e586f86a7ef
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-12 00:50:30 -0500

    CRUD compras completo

---
+++ Repositorio1/Controllers/CompraController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+
+namespace Repositorio1.Controllers
+{
+    public class CompraController : Controller
+    {
+        // GET: Compra
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021Entities1())
+                return View(db.compra.ToList());
+        }
+        public static string Nombreproveedor(int idProveedor)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(compra newCompra)
+        {
+            if (ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    db.compra.Add(newCompra);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                var compra = db.compra.Find(id);
+                db.compra.Remove(compra);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario2021Entities1())
+            {
+                compra compraDetalle = db.compra.Where(a => a.id == id).FirstOrDefault();
+                return View(compraDetalle);
+            }
+        }
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    compra compra = db.compra.Where(a => a.id == id).FirstOrDefault();
+                    return View(compra);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "ERROR" + ex);
+                return View();
+            }
+        }
+        public ActionResult Edit(compra compraEdit)
+        {
+            try
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    var compra = db.compra.Find(compraEdit.id);
+                    compra.fecha = compraEdit.fecha;
+                    compra.total = compraEdit.total;
+                    compra.id_usuario = compraEdit.id_usuario;
+                    compra.cliente = compraEdit.cliente;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error" + ex);
+                return View();
+            }
+        }
+    }
+}
