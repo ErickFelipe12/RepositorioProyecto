ee97e024a47c50c16b1a3c7548c6073cb596ef19
ErickFelipe12 <erick.felipep12@gmail.com>
2021-06-12 02:27:23 -0500

    CRUD completo Usuariorol

---
+++ Repositorio1/Controllers/usuariorolController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Repositorio1.Models;
+
+namespace Repositorio1.Controllers
+{
+    public class usuariorolController : Controller
+    {
+        // GET: usuariorol
+
+        public class producto_compraController : Controller
+        {
+            // GET: producto_compra
+            public ActionResult Index()
+            {
+                using (var db = new inventario2021Entities1())
+                    return View(db.usuariorol.ToList());
+            }
+            public ActionResult Create()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            [ValidateAntiForgeryToken]
+            public ActionResult Create(usuariorol newusuariorol)
+            {
+                if (ModelState.IsValid)
+                    return View();
+
+                try
+                {
+                    using (var db = new inventario2021Entities1())
+                    {
+                        db.usuariorol.Add(newusuariorol);
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error" + ex);
+                    return View();
+                }
+            }
+
+            public ActionResult Delete(int id)
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    var usuariorol = db.usuariorol.Find(id);
+                    db.usuariorol.Remove(usuariorol);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            public ActionResult Details(int id)
+            {
+                using (var db = new inventario2021Entities1())
+                {
+                    usuariorol usuariorolDetalle = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                    return View(usuariorolDetalle);
+                }
+
+
+            }
+            public ActionResult Edit(int id)
+            {
+                try
+                {
+                    using (var db = new inventario2021Entities1())
+                    {
+                        usuariorol usuariorol = db.usuariorol.Where(a => a.id == id).FirstOrDefault();
+                        return View(usuariorol);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "ERROR" + ex);
+                    return View();
+                }
+            }
+            public ActionResult Edit(usuariorol usuariorolEdit)
+            {
+                try
+                {
+                    using (var db = new inventario2021Entities1())
+                    {
+                        var usuariorol = db.usuariorol.Find(usuariorolEdit.id);
+                        usuariorol.idUsuario = usuariorolEdit.idUsuario;
+                        usuariorol.idUsuario = usuariorolEdit.idUsuario;
+                        usuariorol.idRol = usuariorolEdit.idRol;
+
+
+                        db.SaveChanges();
+                        return RedirectToAction("Index");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError("", "error" + ex);
+                    return View();
+                }
+            }
+
+        }
+    }
+}
